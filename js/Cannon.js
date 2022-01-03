943b7f2f70e5d6cdc2d3c9e039cea4b4948782cd
suhaask31 <92918926+suhaask31@users.noreply.github.com>
2022-01-03 21:10:12 +1100

    Add files via upload

---
+++ js/Cannon.js
@@ -0,0 +1,21 @@
+
+class Cannon {
+
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    this.cannonImg = loadImage("assets/cannon.png") 
+    this.cannonbaseImg = loadImage("assets/cannonbase.png") 
+  }
+  display(){
+    push()
+    rectMode(CENTER)
+    image(this.cannonImg,this.x,this.y,this.width,this.height,this.angle)
+    pop()
+    image(this.cannonbaseImg,70,20,200,200)
+    noFill()
+  }
+}
